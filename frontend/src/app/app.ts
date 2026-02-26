c0fe8c98d2f5e3b41928af1d977cb6f481a2881f
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-02-26 17:37:45 -0300

    feat: create footer and cookies banner

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
+import { CookieBanner } from './shared/components/cookie-banner/cookie-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer, CookieBanner],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
