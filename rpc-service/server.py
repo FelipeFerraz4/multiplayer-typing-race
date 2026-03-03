656dc445f979149b2ecbb5ab214a21eb8cf68826
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-03-02 21:49:36 -0300

    feat: add room service function in the RPCService

--- rpc-service/server.py
+++ rpc-service/server.py
@@ -3,18 +3,41 @@ from rpyc.utils.classic import obtain
 from rpyc.utils.server import ThreadedServer
 from services.room_service import RoomService
 
-
 class RPCService(rpyc.Service):
-    def exposed_create_room(self, user_proxy):
-        # Transforma o proxy enviado pelo Flask em um dict real
-        user = obtain(user_proxy) 
-        
-        service = RoomService()
-        return service.create_room(user)    
 
-    def exposed_join_room(self, room_id, user):
+    def exposed_create_room(self, user_proxy):
+        user = obtain(user_proxy)
+
         service = RoomService()
-        return service.join_room(room_id, user)
+        return service.create_room(user)
+
+
+    def exposed_join_room(self, room_code_proxy, user_proxy):
+        room_code = obtain(room_code_proxy)
+        user = obtain(user_proxy)
+
+        service = RoomService()
+        return service.join_room(room_code, user)
+
+
+    def exposed_get_room(self, room_id_proxy):
+        room_id = obtain(room_id_proxy)
+
+        service = RoomService()
+        return service.get_room(room_id)
+
+
+    def exposed_leave_room(self, room_id_proxy, user_id_proxy):
+        room_id = obtain(room_id_proxy)
+        user_id = obtain(user_id_proxy)
+
+        service = RoomService()
+        return service.leave_room(room_id, user_id)
+
+
+    def exposed_list_rooms(self):
+        service = RoomService()
+        return service.list_rooms()
 
 
 if __name__ == "__main__":
